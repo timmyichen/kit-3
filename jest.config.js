c1349ca89c8cdb91086f590afbf97dcdd69702db
timmyichen <timmy.i.chen@gmail.com>
2019-09-10 09:33:04 +0000

    customize ts-jest diagnostics

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,14 @@ module.exports = {
   moduleNameMapper: {
     '^server/(.*)': '<rootDir>/server/$1',
   },
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        warnOnly: true,
+        // https://github.com/kulshekhar/ts-jest/blob/master/docs/user/config/diagnostics.md#advanced-configuration
+        // 2339 pops up on every test in travis for some reason
+        ignoreCodes: [6059, 18002, 18003, 2339],
+      },
+    },
+  },
 };
